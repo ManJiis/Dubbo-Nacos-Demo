c522dcc55b75c81209aa7b076e05694e7067ff5d
tanglinghan <im.linghan@qq.com>
2020-11-30 17:31:28 +0800

    统一返回对象JsonResult 重命名为 ResultInfo

--- service-common/src/main/java/cn/tlh/ex/common/vo/resp/ResultInfo.java
+++ service-common/src/main/java/cn/tlh/ex/common/vo/resp/ResultInfo.java
@@ -5,10 +5,11 @@ import cn.tlh.ex.common.exception.BusinessMsgEnum;
 
 /**
  * 统一返回对象
- * @author ling
+ *
  * @param <T>
+ * @author ling
  */
-public class JsonResult<T> {
+public class ResultInfo<T> {
 
     private T data;
     private String code;
@@ -17,26 +18,28 @@ public class JsonResult<T> {
     /**
      * 若没有数据返回，默认状态码为0，提示信息为：操作成功！
      */
-    public JsonResult() {
+    public ResultInfo() {
         this.code = "0";
         this.msg = "操作成功！";
     }
 
     /**
      * 若没有数据返回，可以人为指定状态码和提示信息
+     *
      * @param code
      * @param msg
      */
-    public JsonResult(String code, String msg) {
+    public ResultInfo(String code, String msg) {
         this.code = code;
         this.msg = msg;
     }
 
     /**
      * 有数据返回时，状态码为0，默认提示信息为：操作成功！
+     *
      * @param data
      */
-    public JsonResult(T data) {
+    public ResultInfo(T data) {
         this.data = data;
         this.code = "0";
         this.msg = "操作成功！";
@@ -44,20 +47,34 @@ public class JsonResult<T> {
 
     /**
      * 有数据返回，状态码为0，人为指定提示信息
+     *
      * @param data
      * @param msg
      */
-    public JsonResult(T data, String msg) {
+    public ResultInfo(T data, String msg) {
         this.data = data;
         this.code = "0";
         this.msg = msg;
     }
 
     /**
+     * 手动指定返回数据,状态码,返回消息
+     *
+     * @param data
+     * @param msg
+     */
+    public ResultInfo(T data, String code, String msg) {
+        this.data = data;
+        this.code = code;
+        this.msg = msg;
+    }
+
+    /**
      * 使用自定义异常作为参数传递状态码和提示信息
+     *
      * @param msgEnum
      */
-    public JsonResult(BusinessMsgEnum msgEnum) {
+    public ResultInfo(BusinessMsgEnum msgEnum) {
         this.code = msgEnum.code();
         this.msg = msgEnum.msg();
     }
