a34a513f062f4625cafcb9f764d3136590df3ec3
tanglinghan <im.linghan@gmail.com>
2020-12-06 21:10:12 +0800

    添加超时订单处理器

---
+++ dubbo-provide/src/main/java/cn/tlh/ex/provide/mq/processor/OrderTimeoutProcessor.java
@@ -0,0 +1,55 @@
+package cn.tlh.ex.provide.mq.processor;
+
+
+import cn.tlh.ex.common.entity.DlxMessage;
+import cn.tlh.ex.common.entity.Order;
+import cn.tlh.ex.common.util.Constant;
+import cn.tlh.ex.dao.OrderDao;
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * 消费订单延迟队列中的消息
+ *
+ * @author TANG
+ */
+@Component
+public class OrderTimeoutProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderTimeoutProcessor.class);
+
+    @Resource
+    OrderDao orderDao;
+
+    @RabbitHandler
+    @RabbitListener(queues = Constant.ORDER_TIMEOUT_QUEUE)
+    public void process(String content, Channel channel, Message messages) {
+        try {
+            DlxMessage message = JSON.parseObject(content, DlxMessage.class);
+            Order order = new Order();
+            // 交易状态 0:待支付 1:交易成功 2:交易失败
+            order.setState(2);
+            order.setErrorMsg("取消订单");
+            UpdateWrapper<Order> updateWrapper = new UpdateWrapper<>();
+            orderDao.update(order, updateWrapper.eq("id", message.getContent()));
+            System.out.println("超时订单 订单号: [" + message.getContent() + "] 取消完毕......");
+            /*
+             *  第二个参数取值为 false 时，表示通知 RabbitMQ 当前消息被确认
+             *  如果为 true，则额外将比第一个参数指定的 delivery tag小的消息一并确认
+             */
+            channel.basicAck(messages.getMessageProperties().getDeliveryTag(), false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
