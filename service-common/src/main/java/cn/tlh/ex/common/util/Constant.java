9313acb6bafcc9cb82a5ff22be28f621b43cfada
tanglinghan <im.linghan@gmail.com>
2020-12-06 21:13:31 +0800

    添加常量类和雪花算法ID生成器

--- service-common/src/main/java/cn/tlh/ex/common/util/Constant.java
+++ service-common/src/main/java/cn/tlh/ex/common/util/Constant.java
@@ -7,6 +7,48 @@ package cn.tlh.ex.common.util;
  * @version: 1.0
  */
 public class Constant {
+    /**
+     * 发送中
+     */
+    public static final String ORDER_SENDING = "0";
+
+    /**
+     * 成功
+     */
+    public static final String ORDER_SEND_SUCCESS = "1";
+
+    /**
+     * 失败
+     */
+    public static final String ORDER_SEND_FAILURE = "-1";
+    /**
+     * 分钟超时单位：min
+     */
+    public static final int ORDER_TIMEOUT = 1;
+
+
+    //-------------------------------- 订单延迟队列 start --------------------------------//
+    /**
+     * 订单延迟队列
+     */
+    public static final String ORDER_DELAY_QUEUE = "dev.order.queue";
+    /**
+     * 订单exchange
+     */
+    public static final String ORDER_EXCHANGE = "dev.order.exchange";
+    /**
+     * 死信交换机
+     */
+    public static final String ORDER_DLX_EXCHANGE = "dev.order.dlx.exchange";
+    /**
+     * 超时订单任务队列
+     */
+    public static final String ORDER_TIMEOUT_QUEUE = "dev.order.timeout.queue";
+    /**
+     * 过期消息路由key
+     */
+    public static final String ORDER_DLK_KEY = "dev.order.delay";
+    //-------------------------------- 订单延迟队列 end --------------------------------//
 
 
 }
