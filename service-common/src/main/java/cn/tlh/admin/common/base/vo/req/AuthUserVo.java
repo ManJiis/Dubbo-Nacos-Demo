37cce4a0a7dbf572172378a4ebffe58deee072f1
tanglinghan <im.linghan@qq.com>
2020-12-23 08:59:29 +0800

    添加认证请求对象

--- service-common/src/main/java/cn/tlh/admin/common/base/vo/req/AuthUserVo.java
+++ service-common/src/main/java/cn/tlh/admin/common/base/vo/req/AuthUserVo.java
@@ -6,14 +6,17 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 
 
+/**
+ * @author TANG
+ */
 @Getter
 @Setter
 public class AuthUserVo {
 
-    @NotBlank
+    @NotBlank(message = "登录账号不能为空")
     private String username;
 
-    @NotBlank
+    @NotBlank(message = "登录密码不能为空")
     private String password;
 
     private String code;
