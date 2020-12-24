294955f551fc2bc71500717da47586a3e8093b11
tanglinghan <im.linghan@qq.com>
2020-12-24 16:22:35 +0800

    更新CodeEnum命名

--- service-common/src/main/java/cn/tlh/admin/common/util/enums/CodeEnum.java
+++ service-common/src/main/java/cn/tlh/admin/common/util/enums/CodeEnum.java
@@ -16,16 +16,16 @@ import lombok.Getter;
 public enum CodeEnum {
 
     /* 通过手机号码重置邮箱 */
-    PHONE_RESET_EMAIL_CODE("phone_reset_email_code_", "通过手机号码重置邮箱"),
+    PHONE_RESET_EMAIL_CODE("reset:email:byPhone:code:", "通过手机号码重置邮箱"),
 
     /* 通过旧邮箱重置邮箱 */
-    EMAIL_RESET_EMAIL_CODE("email_reset_email_code_", "通过旧邮箱重置邮箱"),
+    EMAIL_RESET_EMAIL_CODE("reset:email:byEmail:code:", "通过旧邮箱重置邮箱"),
 
     /* 通过手机号码重置密码 */
-    PHONE_RESET_PWD_CODE("phone_reset_pwd_code_", "通过手机号码重置密码"),
+    PHONE_RESET_PWD_CODE("reset:pwd:byPhone:code:", "通过手机号码重置密码"),
 
     /* 通过邮箱重置密码 */
-    EMAIL_RESET_PWD_CODE("email_reset_pwd_code_", "通过邮箱重置密码");
+    EMAIL_RESET_PWD_CODE("reset:pwd:byEmail:code:", "通过邮箱重置密码");
 
     private final String key;
     private final String description;
