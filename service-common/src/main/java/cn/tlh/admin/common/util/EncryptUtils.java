099738f274a6bbabbb8c2f521041c10aeca46b8a
tanglinghan <im.linghan@gmail.com>
2020-12-27 12:32:03 +0800

    加密工具类添加生成盐方法

--- service-common/src/main/java/cn/tlh/admin/common/util/EncryptUtils.java
+++ service-common/src/main/java/cn/tlh/admin/common/util/EncryptUtils.java
@@ -2,12 +2,18 @@
 
 package cn.tlh.admin.common.util;
 
+import org.apache.commons.codec.binary.Hex;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Objects;
+import java.util.Random;
 
 
 /**
@@ -95,6 +101,78 @@ public class EncryptUtils {
         System.out.println("desDecrypt(s) = " + desDecrypt(s));
         String enPass = "11F2DE5176D49691F9EE948AB7C56E52FE9E07F8F2A5F02B";
         System.out.println("desDecrypt(enPass) = " + desDecrypt(enPass));
+
+        String password = generate("admin");
+        System.out.println("加密后的密码 = " + password);
+        System.out.println(verify("admin", password));
+        String generateSalt = generateSalt();
+        System.out.println("generateSalt = " + generateSalt);
     }
+
+    private static final String ORGIN_STR = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static String generateSalt() {
+        StringBuilder builder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < 20; i++) {
+            builder.append(ORGIN_STR.charAt(secureRandom.nextInt(ORGIN_STR.length())));
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 生成含有随机盐的密码
+     */
+    public static String generate(String password) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder(16);
+        sb.append(r.nextInt(99999999)).append(r.nextInt(99999999));
+        int len = sb.length();
+        if (len < 16) {
+            for (int i = 0; i < 16 - len; i++) {
+                sb.append("0");
+            }
+        }
+        String salt = sb.toString();
+        password = md5Hex(password + salt);
+        char[] cs = new char[48];
+        for (int i = 0; i < 48; i += 3) {
+            assert password != null;
+            cs[i] = password.charAt(i / 3 * 2);
+            char c = salt.charAt(i / 3);
+            cs[i + 1] = c;
+            cs[i + 2] = password.charAt(i / 3 * 2 + 1);
+        }
+        return new String(cs);
+    }
+
+    /**
+     * 校验密码是否正确
+     */
+    public static boolean verify(String password, String md5) {
+        char[] cs1 = new char[32];
+        char[] cs2 = new char[16];
+        for (int i = 0; i < 48; i += 3) {
+            cs1[i / 3 * 2] = md5.charAt(i);
+            cs1[i / 3 * 2 + 1] = md5.charAt(i + 2);
+            cs2[i / 3] = md5.charAt(i + 1);
+        }
+        String salt = new String(cs2);
+        return Objects.equals(md5Hex(password + salt), new String(cs1));
+    }
+
+    /**
+     * 获取十六进制字符串形式的MD5摘要
+     */
+    public static String md5Hex(String src) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bs = md5.digest(src.getBytes());
+            return new String(new Hex().encode(bs));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
 
