778b27626bcb1a0b7d6bbd829662757243edab42
tanglinghan <im.linghan@gmail.com>
2020-12-27 10:54:57 +0800

    更新AuthUserVO字段

--- service-common/src/main/java/cn/tlh/admin/common/base/vo/req/AuthUserVo.java
+++ service-common/src/main/java/cn/tlh/admin/common/base/vo/req/AuthUserVo.java
@@ -1,5 +1,7 @@
 package cn.tlh.admin.common.base.vo.req;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,15 +13,22 @@ import javax.validation.constraints.NotBlank;
  */
 @Getter
 @Setter
+@ApiModel("登录请求对象")
 public class AuthUserVo {
 
     @NotBlank(message = "登录账号不能为空")
-    private String username;
+    @ApiModelProperty("登录账号")
+    private String loginAccount;
 
     @NotBlank(message = "登录密码不能为空")
+    @ApiModelProperty("登录密码")
     private String password;
 
+    @ApiModelProperty("验证码")
     private String code;
+
+    @ApiModelProperty("动态码")
+    private String secCode;
 
     private String uuid = "";
 }
