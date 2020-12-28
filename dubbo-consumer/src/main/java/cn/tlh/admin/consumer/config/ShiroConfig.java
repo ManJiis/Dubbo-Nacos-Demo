0383296fd4441241a71d24f5f32a4bf584433453
tanglinghan <im.linghan@qq.com>
2020-12-28 15:14:12 +0800

    ShiroConfig修改

--- dubbo-consumer/src/main/java/cn/tlh/admin/consumer/config/ShiroConfig.java
+++ dubbo-consumer/src/main/java/cn/tlh/admin/consumer/config/ShiroConfig.java
@@ -1,11 +1,13 @@
 package cn.tlh.admin.consumer.config;
 
 import cn.tlh.admin.consumer.shiro.MySessionManager;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.mgt.SessionsSecurityManager;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.crazycake.shiro.RedisCacheManager;
 import org.crazycake.shiro.RedisSessionDAO;
@@ -14,7 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author TANG
@@ -24,18 +29,18 @@ import java.util.List;
 @Configuration
 public class ShiroConfig {
 
-    @Autowired
+    @Autowired(required = false)
     private RedisSessionDAO redisSessionDAO;
-    @Autowired
+    @Autowired(required = false)
     private RedisCacheManager redisCacheManager;
 
     @Bean
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
         DefaultShiroFilterChainDefinition chain = new DefaultShiroFilterChainDefinition();
-        // 配置退出 过滤器, 登出后跳转配置的loginUrl
         // 配置不会被拦截的链接 顺序判断
         chain.addPathDefinition("/**", "anon");
-/*        chain.addPathDefinition("/401", "anon");
+/*
+        chain.addPathDefinition("/401", "anon");
         chain.addPathDefinition("/403", "anon");
         chain.addPathDefinition("/swagger/**", "anon");
         chain.addPathDefinition("/v2/**", "anon");
@@ -44,25 +49,13 @@ public class ShiroConfig {
         chain.addPathDefinition("/swagger-ui.html", "anon");
         chain.addPathDefinition("/doc.html/**", "anon");
         chain.addPathDefinition("/login", "anon");
-        chain.addPathDefinition("/actuator/**", "anon");
-        chain.addPathDefinition("/favicon.ico", "anon");
-        chain.addPathDefinition("/sysUser/**", "anon");
         chain.addPathDefinition("/error", "anon");
-        chain.addPathDefinition("/merchant/publish/sfOrderState", "anon");
-        chain.addPathDefinition("/merchant/publish/sfRoutePushService", "anon");
-        chain.addPathDefinition("/simTerminal/echoAdmissibleSim", "anon");
-        chain.addPathDefinition("/posTerminal/echoAdmissiblePos", "anon");
-        chain.addPathDefinition("/cardAcceptance/echoAdmissibleCard", "anon");
-        chain.addPathDefinition("/orderResp/echoOrderResp", "anon");
-        chain.addPathDefinition("/orderResp/echoIndustry", "anon");
-        chain.addPathDefinition("/respBank/bankFeedback", "anon");
-        chain.addPathDefinition("/manualReview/**", "anon");
-        chain.addPathDefinition("/apiManager/**", "anon");
-        chain.addPathDefinition("/jiangsu/**", "anon");
         chain.addPathDefinition("/logout", "anon");
-        chain.addPathDefinition("/**", "authc");*/
+        chain.addPathDefinition("/**", "authc");
+*/
         return chain;
     }
+
 
     @Bean
     public SessionManager sessionManager() {
@@ -71,9 +64,30 @@ public class ShiroConfig {
         // 不在地址栏显示sessionId
         sessionManager.setSessionIdUrlRewritingEnabled(false);
         sessionManager.setSessionValidationSchedulerEnabled(true);
-        //设置超时时间
-        sessionManager.setGlobalSessionTimeout(3600000L);
+        // 设置超时时间
+        sessionManager.setGlobalSessionTimeout(60 * 60 * 24 * 7 * 1000L);
         return sessionManager;
+    }
+
+    /**
+     * RedisSessionDAO shiro sessionDao层的实现 通过redis
+     * 使用的是shiro-redis开源插件
+     */
+    @Bean
+    public RedisSessionDAO redisSessionDAO() {
+        RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
+//        redisSessionDAO.setRedisManager(redisManager());
+        return redisSessionDAO;
+    }
+
+    /**
+     * cacheManager 缓存 redis实现
+     * 使用的是shiro-redis开源插件
+     */
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheManager redisCacheManager = new RedisCacheManager();
+//        redisCacheManager.setRedisManager(redisManager());
+        return redisCacheManager;
     }
 
     @Bean
@@ -85,7 +99,8 @@ public class ShiroConfig {
     }
 
     /**
-     * 开启Shiro的注解(如@RequiresRoles,//@RequiresPermissions),需借助SpringAOP
+     * 开启Shiro的注解
+     * （如@RequiresRoles,@RequiresPermissions）,需借助SpringAOP
      */
     @Bean
     public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
