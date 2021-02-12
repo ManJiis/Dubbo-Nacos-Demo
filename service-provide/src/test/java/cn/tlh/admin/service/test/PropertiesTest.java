5f9c87f4d477738d83697e0c5e05c9b48cd0bf52
tlh <im.linghan@qq.com>
2021-02-13 01:10:36 +0800

    add: common模块添加Properties配置文件内容读取测试

---
+++ service-provide/src/test/java/cn/tlh/admin/service/test/PropertiesTest.java
@@ -0,0 +1,26 @@
+package cn.tlh.admin.service.test;
+
+import cn.tlh.admin.common.util.ActEnvProperties;
+import cn.tlh.admin.service.serviceImpl.ProvideApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Properties;
+
+/**
+ * @author TANG
+ * @description propertise测试
+ * @date 2021-02-13
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProvideApplication.class)
+public class PropertiesTest {
+
+    @Test
+    public void test1() {
+        Properties properties = ActEnvProperties.getProperties();
+        System.out.println("current ActEnv = " + properties.getProperty("ActEnv"));
+    }
+}
